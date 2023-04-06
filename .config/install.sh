2091263b4c36a7ed82ccfc763b22c9f4f6ab8fac
Ali Foroughi <Foroughi.Ali@gmail.com>
2023-04-06 23:25:41 +0200

    removed extra files like conky

--- .config/install.sh
+++ .config/install.sh
@@ -10,12 +10,10 @@ sudo apt -y --fix-broken install
 rm google-chrome-stable_current_amd64.deb 
 
 sudo apt install -y cmake build-essential ranger compton rofi unzip alsa-utils nitrogen gnome-keyring network-manager-gnome htop
-sudo apt install -y libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev conky pavucontrol ntfs-3g firmware-realtek scrot
+sudo apt install -y libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev pavucontrol ntfs-3g firmware-realtek scrot
 
 mkdir ~/.fonts
 
-sudo apt install -y python3-pip
-pip3 install requests
 
 cp ~/.config/fonts/* ~/.fonts
 fc-cache -f -v
